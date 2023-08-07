acab10ca58e8d1cc5409a7f59b941b5fb5910c0c
Gustavo <tavooski@users.noreply.github.com>
2023-08-07 12:46:11 -0700

    Updated UI in seTimebox with new icons and ‘SELECT’ in the dropdown button

--- lib/timebox.dart
+++ lib/timebox.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const List<String> list = <String>[
+  'SELECT',
   '< 1 Week',
   '1 Week',
   '2 Weeks',
@@ -54,15 +55,16 @@ class _TimeboxState extends State<Timebox> {
         crossAxisCount: 2,
         children: <Widget>[
           Image.asset(
-              key: Key('sprintdaysIcon'),
+              key: Key('sprintdurationIcon'),
               'assets/timebox/sprintduration.png',
-              width: 50,
-              height: 50),
+              width: 75,
+              height: 75),
           Container(
             alignment: Alignment.center,
             child: DropdownButton<String>(
               key: Key('sprintdurationDropdown'),
-              style: TextStyle(fontFamily: 'Shrikhand', color: Colors.black, fontSize: 20),
+              style: TextStyle(
+                  fontFamily: 'Shrikhand', color: Colors.black, fontSize: 20),
               value: dropdownValue,
               onChanged: (String? value) {
                 setState(() {
@@ -75,56 +77,67 @@ class _TimeboxState extends State<Timebox> {
               }).toList(),
             ),
           ),
-          Image.asset(
-            key: Key('sprintplanningIcon'),
-            'assets/timebox/sprintplanning.png',
-            width: 150,
-            height: 90,
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              key: Key('sprintplanningText'),
-              style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
-              '${sprintPlanningTimeboxEstimates[dropdownValue]}',
+          if (dropdownValue != 'SELECT') ...[
+            Image.asset(
+              key: Key('sprintplanningIcon'),
+              'assets/timebox/sprintplanning.png',
+              width: 75,
+              height: 75,
             ),
-          ),
-          Image.asset(
-            key: Key('dailyscrumIcon'),
-            'assets/timebox/dailyscrum.png',
-            width: 150,
-            height: 90,
-          ),
-          Container(
-              alignment: Alignment.center,
-              child: Text(
-                  key: Key('dailyscrumText'),
-                  style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
-                  '15 mins')),
-          Image.asset(
-            key: Key('sprintreviewIcon'),
-            'assets/timebox/sprintreview.png',
-            width: 150,
-            height: 90,
-          ),
-          Container(
-              alignment: Alignment.center,
-              child: Text(
-                  key: Key('sprintreviewText'),
-                  style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
-                  '${sprintReviewTimeboxEstimates[dropdownValue]}')),
-          Image.asset(
-            key: Key('sprintretroIcon'),
-            'assets/timebox/sprintretro.png',
-            width: 150,
-            height: 90,
-          ),
-          Container(
-              alignment: Alignment.center,
-              child: Text(
-                  key: Key('sprintretroText'),
-                  style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
-                  '${sprintRetroTimeboxEstimates[dropdownValue]}')),
+            Column(
+              children: [
+                Text(
+                  'Maximum Duration',
+                  style:TextStyle(fontFamily: 'Shrikhand', fontSize: 18),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 32, 0, 0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    key: Key('sprintplanningText'),
+                    style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+                    '${sprintPlanningTimeboxEstimates[dropdownValue]}',
+                  ),
+                ),
+              ],
+            ),
+            Image.asset(
+              key: Key('dailyscrumIcon'),
+              'assets/timebox/dailyscrum.png',
+              width: 75,
+              height: 75,
+            ),
+            Container(
+                alignment: Alignment.center,
+                child: Text(
+                    key: Key('dailyscrumText'),
+                    style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+                    '15 mins')),
+            Image.asset(
+              key: Key('sprintreviewIcon'),
+              'assets/timebox/sprintreview.png',
+              width: 75,
+              height: 75,
+            ),
+            Container(
+                alignment: Alignment.center,
+                child: Text(
+                    key: Key('sprintreviewText'),
+                    style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+                    '${sprintReviewTimeboxEstimates[dropdownValue]}')),
+            Image.asset(
+              key: Key('sprintretroIcon'),
+              'assets/timebox/sprintretro.png',
+              width: 75,
+              height: 75,
+            ),
+            Container(
+                alignment: Alignment.center,
+                child: Text(
+                    key: Key('sprintretroText'),
+                    style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+                    '${sprintRetroTimeboxEstimates[dropdownValue]}')),
+          ],
         ],
       ),
     );
