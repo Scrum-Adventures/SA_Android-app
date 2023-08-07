1253102cb4f19ac2cefe73a32677cdca4e56c804
Gustavo <tavooski@users.noreply.github.com>
2023-08-07 09:40:15 -0700

    updated the HowToPlay screen and made some UI changes to the seTimebox feature

--- lib/playInfo.dart
+++ lib/playInfo.dart
@@ -5,8 +5,8 @@ class PlayInfo extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
           title: Center(
-            child: Text('How to play'),
-          )),
+        child: Text('How to play'),
+      )),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(10),
         child: RichText(
@@ -20,7 +20,7 @@ class PlayInfo extends StatelessWidget {
               children: <TextSpan>[
                 TextSpan(
                   text:
-                  'For estimating a product backlog item relative size, choose either the t-shirt or Fibonacci card option. For voting situations, select the stoplight option to communicate your view. To determine the team capacity for a Sprint, use the Capacity Calculator.',
+                      'For estimating a product backlog item relative size, choose either the t-shirt or Fibonacci card option. For voting situations, select the stoplight option to communicate your view. To determine the team capacity for a Sprint, use the Capacity Calculator. To determine the timebox for each Scrum Event, use the Scrum Events Timebox.',
                   style: TextStyle(
                     color: Color.fromRGBO(47, 86, 163, 1),
                     fontSize: 20,
@@ -36,7 +36,7 @@ class PlayInfo extends StatelessWidget {
                 ),
                 TextSpan(
                   text:
-                  '\nPick a t-shirt size to estimate the relative sizing of the selected product backlog item and show your t-shirt to the team.',
+                      '\nPick a t-shirt size to estimate the relative sizing of the selected product backlog item and show your t-shirt to the team.',
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 20,
@@ -51,7 +51,7 @@ class PlayInfo extends StatelessWidget {
                 ),
                 TextSpan(
                   text:
-                  '\nChoose a Fibonacci card to estimate the relative sizing of the selected product backlog item and show your card to the team.',
+                      '\nChoose a Fibonacci card to estimate the relative sizing of the selected product backlog item and show your card to the team.',
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 20,
@@ -66,7 +66,7 @@ class PlayInfo extends StatelessWidget {
                 ),
                 TextSpan(
                   text:
-                  '\nSelect a stoplight color to indicate the following:\nGreen = Thumbs up / Agree\nYellow = Not sure / Need further discussion \nRed = Thumbs down / Do not agree',
+                      '\nSelect a stoplight color to indicate the following:\nGreen = Thumbs up / Agree\nYellow = Not sure / Need further discussion \nRed = Thumbs down / Do not agree',
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 20,
@@ -81,7 +81,7 @@ class PlayInfo extends StatelessWidget {
                 ),
                 TextSpan(
                   text:
-                  '\nSelect one team member to collect and enter each team  member’s  name  and  their  available  hours  to work  on  the  Sprint.  Once  all  of  the  team  member entries  are  completed,  the  total  team  hours  will  be calculated and made available as the team capacity.',
+                      '\nSelect one team member to collect and enter each team  member’s  name  and  their  available  hours  to work  on  the  Sprint.  Once  all  of  the  team  member entries  are  completed,  the  total  team  hours  will  be calculated and made available as the team capacity.',
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 20,
@@ -96,7 +96,7 @@ class PlayInfo extends StatelessWidget {
                 ),
                 TextSpan(
                   text:
-                  '\nEnter the number of days in your next sprint and the app will calculate the appropriate timebox for each one of the 5 scrum events. ',
+                      '\nSelect the duration of the Sprint to determine the maximum timebox for the other Scrum Events.',
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 20,
--- lib/timebox.dart
+++ lib/timebox.dart
@@ -36,7 +36,7 @@ class _TimeboxState extends State<Timebox> {
   final Map<String, String> sprintRetroTimeboxEstimates = {
     '< 1 Week': '30 mins',
     '1 Week': '45 mins',
-    '2 Weeks': '1 1/2 hrs',
+    '2 Weeks': '1.5 hrs',
     '3 Weeks': '2 hrs',
     '1 Month': '3 hrs'
   };
@@ -45,17 +45,24 @@ class _TimeboxState extends State<Timebox> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Scrum Events Calculator'),
+        title: Text('Scrum Events Timebox'),
       ),
       body: GridView.count(
         key: Key('timeboxGrid'),
+        physics: NeverScrollableScrollPhysics(),
+        childAspectRatio: 1.30,
         crossAxisCount: 2,
         children: <Widget>[
-          Image.asset(key: Key('sprintdaysIcon'),'assets/timebox/sprintdays.png', width: 50, height: 50),
+          Image.asset(
+              key: Key('sprintdaysIcon'),
+              'assets/timebox/sprintduration.png',
+              width: 50,
+              height: 50),
           Container(
-            padding: EdgeInsets.fromLTRB(45, 70, 12, 12),
+            alignment: Alignment.center,
             child: DropdownButton<String>(
               key: Key('sprintdurationDropdown'),
+              style: TextStyle(fontFamily: 'Shrikhand', color: Colors.black, fontSize: 20),
               value: dropdownValue,
               onChanged: (String? value) {
                 setState(() {
@@ -74,28 +81,50 @@ class _TimeboxState extends State<Timebox> {
             width: 150,
             height: 90,
           ),
-          Text(key: Key('sprintplanningText'),'Sprint planning time: ${sprintPlanningTimeboxEstimates[dropdownValue]}'),
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              key: Key('sprintplanningText'),
+              style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+              '${sprintPlanningTimeboxEstimates[dropdownValue]}',
+            ),
+          ),
           Image.asset(
             key: Key('dailyscrumIcon'),
             'assets/timebox/dailyscrum.png',
             width: 150,
             height: 90,
           ),
-          Text(key: Key('dailyscrumText'),'15 minutes'),
+          Container(
+              alignment: Alignment.center,
+              child: Text(
+                  key: Key('dailyscrumText'),
+                  style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+                  '15 mins')),
           Image.asset(
             key: Key('sprintreviewIcon'),
             'assets/timebox/sprintreview.png',
             width: 150,
             height: 90,
           ),
-          Text(key: Key('sprintreviewText'),'Sprint review time: ${sprintReviewTimeboxEstimates[dropdownValue]}'),
+          Container(
+              alignment: Alignment.center,
+              child: Text(
+                  key: Key('sprintreviewText'),
+                  style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+                  '${sprintReviewTimeboxEstimates[dropdownValue]}')),
           Image.asset(
             key: Key('sprintretroIcon'),
             'assets/timebox/sprintretro.png',
             width: 150,
             height: 90,
           ),
-          Text(key: Key('sprintretroText'),'Sprint retro time: ${sprintRetroTimeboxEstimates[dropdownValue]}'),
+          Container(
+              alignment: Alignment.center,
+              child: Text(
+                  key: Key('sprintretroText'),
+                  style: TextStyle(fontFamily: 'Shrikhand', fontSize: 28),
+                  '${sprintRetroTimeboxEstimates[dropdownValue]}')),
         ],
       ),
     );
